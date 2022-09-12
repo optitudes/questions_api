6340ea36662ed39f7d4fbb8675eb857b2416245a
Juan Sebastian Rojas Garcia <juans@danielapps.co>
2022-09-12 08:58:14 -0500

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Daniel Apps on 12/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/question_apiApp.swift
@@ -0,0 +1,17 @@
+//
+//  question_apiApp.swift
+//  Shared
+//
+//  Created by Daniel Apps on 12/09/22.
+//
+
+import SwiftUI
+
+@main
+struct question_apiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
